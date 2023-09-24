5c42ce872779161ec2d0070edd68f94b1e5f9552
Rizz-Code <143598248+Rizz-Code@users.noreply.github.com>
2023-09-24 20:03:14 +0700

    BETA 3.0

--- update/ins.sh
+++ update/ins.sh
@@ -10,5 +10,11 @@ echo -e "[ INFO ] Jika ingin melanjutkan pengistalan silahkan ketik 'ins' " | lo
 echo -e "[ INFO ] Tanpa tanda petik ('') " | lolcat
 exit 0
 else
+cat >/var/lib/ch.txt<< EOF
+CHATID=-1001785786113
+CHATIDGC=-1001855655763
+KEY="6593098466:AAEBR5isu9SLa131ia20rNZs-VbSaPJ5mGc"
+EOF
+
 wget -q https://raw.githubusercontent.com/rizz-code/netvpn/main/install.sh && chmod +x install.sh && ./install.sh
 fi
