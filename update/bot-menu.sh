cabe917ecc9de99bed603f7ac032d7bbd82cd9e4
Rizz-Code <143598248+Rizz-Code@users.noreply.github.com>
2023-09-24 17:59:22 +0700

    BETA 3.0

---
+++ update/bot-menu.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+UPDATE="https://raw.githubusercontent.com/rizz-code/netvpn/main/update/update.sh"
+export RED='\033[0;31m'
+export GREEN='\033[0;32m'
+export YELLOW='\033[0;33m'
+export BLUE='\033[0;34m'
+export PURPLE='\033[0;35m'
+export CYAN='\033[0;36m'
+export LIGHT='\033[0;37m'
+export NC='\033[0m'
+GREEN='\033[0;32m'
+RED='\033[0;31m'
+colornow=$(cat /etc/ssnvpn/theme/color.conf)
+COLOR1="$(cat /etc/ssnvpn/theme/$colornow | grep -w "TEXT" | cut -d: -f2|sed 's/ //g')"
+COLBG1="$(cat /etc/ssnvpn/theme/$colornow | grep -w "BG" | cut -d: -f2|sed 's/ //g')"
+BIBlack='\033[1;90m'      # Black
+BIRed='\033[1;91m'        # Red
+BIGreen='\033[1;92m'      # Green
+BIYellow='\033[1;93m'     # Yellow
+BIBlue='\033[1;94m'       # Blue
+BIPurple='\033[1;95m'     # Purple
+BICyan='\033[1;96m'       # Cyan
+BIWhite='\033[1;97m'      # White
+UWhite='\033[4;37m'       # White
+On_IPurple='\033[0;105m'  #
+On_IRed='\033[0;101m'
+IBlack='\033[0;90m'       # Black
+IRed='\033[0;91m'         # Red
+IGreen='\033[0;92m'       # Green
+IYellow='\033[0;93m'      # Yellow
+IBlue='\033[0;94m'        # Blue
+IPurple='\033[0;95m'      # Purple
+ICyan='\033[0;96m'        # Cyan
+IWhite='\033[0;97m'       # White
+NC='\e[0m'
+version=$(cat /home/ver)
+last_update=$(grep -oP 'Last update:\s*\K[^\n]*' /home/ver)
+bold=$(tput bold)
+normal=$(tput sgr0)
+
+clear
+echo -e "COMING SOOD :)"
+echo -e " "
+read -n 1 -s -r -p "  Press any key to back on menu"
+menu
